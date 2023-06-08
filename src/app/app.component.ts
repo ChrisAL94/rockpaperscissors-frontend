49953166952b73f65679df6965a74fb23bb9eecf
Christian Albers <christian.albers@tchibo.de>
2023-06-08 20:50:11 +0200

    feat: created header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rockpaperscissors-frontend';
+  buttonClickCount = 0;
+  buttonClicked() {
+    this.buttonClickCount++;
+    alert(`Button was clicked ${this.buttonClickCount} times`)
+  }
 }
