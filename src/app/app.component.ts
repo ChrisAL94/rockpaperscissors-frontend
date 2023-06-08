500cbd9350d5c057c9745f27ca7f18607405fcd9
Christian Albers <christian.albers@tchibo.de>
2023-06-08 21:45:15 +0200

    feat: installed material ui+

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rockpaperscissors-frontend';
-  buttonClickCount = 0;
-  buttonClicked() {
-    this.buttonClickCount++;
-    alert(`Button was clicked ${this.buttonClickCount} times`)
-  }
 }
