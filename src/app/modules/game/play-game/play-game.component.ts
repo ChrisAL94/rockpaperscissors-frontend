307d63c9eefb61f94ff5b5b0b3a4dc8e59ce8fab
Christian Albers <christian.albers@tchibo.de>
2023-06-11 20:36:44 +0200

    feat: Implemented play and spinner for play game

--- src/app/modules/game/play-game/play-game.component.ts
+++ src/app/modules/game/play-game/play-game.component.ts
@@ -10,30 +10,37 @@ import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { UserService } from '../../../services/userService/user.service';
-import { GameSymbols } from '../../../services/gameService/game.service';
+import { GameService, GameSymbols } from '../../../services/gameService/game.service';
 import { OnInit } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-play-game',
   templateUrl: './play-game.component.html',
   styleUrls: ['./play-game.component.css'],
   standalone: true,
-  imports: [MatCardModule, MatDividerModule, MatButtonModule, MatFormFieldModule, MatSelectModule, FormsModule, NgForOf, RouterLink, MatIconModule, MatButtonToggleModule, NgIf],
+  imports: [MatCardModule, MatDividerModule, MatButtonModule, MatFormFieldModule, MatSelectModule, FormsModule, NgForOf, RouterLink, MatIconModule, MatButtonToggleModule, NgIf, MatProgressSpinnerModule],
 })
 export class PlayGameComponent implements OnInit{
   playGame = true;
   gameResult = "You won!";
   computerChoice = GameSymbols.Rock;
   playerChoice: GameSymbols | undefined;
-
   userService: UserService;
+  gameService: GameService;
 
-  constructor(userService: UserService) {
+  constructor(userService: UserService, gameService: GameService) {
     this.userService = userService;
+    this.gameService = gameService;
   }
 
   ngOnInit() {
     this.playerChoice = undefined;
+  }
+
+  async play() {
+    this.playGame = false;
+    this.gameResult = await this.gameService.play(this.playerChoice);
   }
 
 }
--- src/app/services/gameService/game.service.ts
+++ src/app/services/gameService/game.service.ts
@@ -6,10 +6,28 @@ export enum GameSymbols {
   Scissors = "Scissors"
 }
 
+export enum GameResult {
+  Win = "You won!",
+  Defeat = "You Lost!",
+  Tie = "It`s a tie!"
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  loading = false;
 
   constructor() { }
+
+  async play(playerChoice: GameSymbols | undefined) {
+    return new Promise<GameResult>((resolve, reject) => {
+      this.loading = true;
+      setTimeout(() => {
+        resolve(GameResult.Defeat);
+        reject('No Connection to Backend')
+        this.loading = false;
+      }, 3000)
+    });
+  }
 }
