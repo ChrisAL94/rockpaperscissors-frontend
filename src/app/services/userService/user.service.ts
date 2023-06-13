c1aa2b8b50031862c00e377b2078dd92b58bb08b
Christian A <c.albers-eyendorf@web.de>
2023-06-13 15:37:14 +0200

    refactored fetches in userService

--- src/app/services/userService/user.service.ts
+++ src/app/services/userService/user.service.ts
@@ -13,54 +13,26 @@ let UserList: Array<string> = [user1, user2, user3];
 export class UserService {
   loggedUser: string | undefined;
   loading = false;
+  utils: UtilsService;
+  path = "/user"
 
 
   constructor(utils: UtilsService) {
-    let loginCookieValue = utils.readCookieByName(LOGIN_COOKIE_NAME);
+    this.utils = utils
+    let loginCookieValue = this.utils.readCookieByName(LOGIN_COOKIE_NAME);
     this.loggedUser = loginCookieValue ? JSON.parse(loginCookieValue) as string : undefined;
   }
 
   async getAllUsers(): Promise<Array<string>> {
-    return new Promise<Array<string>>(async (resolve, reject) => {
-      this.loading = true;
-      try {
-        let response = await fetch("http://localhost:8080/api/v1/user");
-        if (response.ok) {
-          this.loading = false;
-          response.json().then(users => {
-            resolve(users)
-          })
-        } else {
-          this.loading = false;
-          reject("fetching all users has been executed with status " + response.status)
-        }
-      } catch (err) {
-        this.loading = false;
-        reject(err)
-      }
-    })
+    this.loading = true;
+    return await this.utils.fetchData<Array<string>>(this.path).finally(() => {this.loading = false});
   }
 
-  async registerUser(newUser: string): Promise<String> {
-    return new Promise<String>(async (resolve, reject) => {
-      this.loading = true;
-      try {
-        let response = await fetch("http://localhost:8080/api/v1/user", {
-          method: "POST",
-          body: newUser
-        });
-        if (response.ok) {
-          this.login(newUser);
-          this.loading = false;
-          resolve(newUser)
-        } else {
-          this.loading = false;
-          reject(JSON.parse(await response.text()).message)
-        }
-      } catch (err) {
-        this.loading = false;
-        reject(err)
-      }
+  async registerUser(newUser: string): Promise<string> {
+    this.loading = true;
+    return await this.utils.fetchData<string>(this.path, {method: "POST", body: newUser}).finally(() => {
+      this.loggedUser = newUser;
+      this.loading = false;
     })
   }
 
